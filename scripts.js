5fec661c35bade5a0819b0a71dcc1d85af0fa36d
Martia <pureromancebymartia@gmail.com>
2022-04-10 19:10:03 -0400

    push

---
+++ scripts.js
@@ -0,0 +1,61 @@
+alert("Helllo, World!");
+//the second alert
+alert("Hello, World!");
+var myVarMartia;
+myVariableMartia=10
+var myVar2="hello";
+myVar2
+'hello'
+myVar2="hello again";
+'hello again'
+myVar2
+myVar2=5;
+var customerAge=32;
+customerAge;
+var myVar3=40;
+ myVar3=40;
+
+
+var myVar=5;
+myVar
+5+5
+myVar +10
+myVar
+myVar =myVar+10
+myVar=myVar=10
+myVar = myVar+5
+myVar
+10-5
+myVar -3
+myVar = myVar -5
+10 * 10
+20/20
+myVar = myVar/2
+myVar
+5+ "hello"
+5= "hello"
+100 + "hello"
+"hello" + "david"
+"hello" + " david"
+5 * "hello"
+
+
+var myVar=10
+myVar = myVar +5
+myVar
+myVar += 5
+myVar
+myVar -= 10
+myVar=10
+myVar=10
+myVar /=2
+myVar*=5
+myVar += 1
+myVar++
+myVar
+++myVar
+
+var myVar = "Martia"
+document.write(myVar)
+document.write(10)
+document.write(20/5)
